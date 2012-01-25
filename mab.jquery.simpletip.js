a4820fd553086cf724236fc56a888ae9fd0bfc75
Mark Bell <me@markashleybell.com>
2012-01-25 13:26:35 +0000

    Cope with multiple tooltip styles within one page

--- mab.jquery.simpletip.js
+++ mab.jquery.simpletip.js
@@ -18,24 +18,26 @@
         var config = { 
             'toolTipId': 'tooltip',
             'toolTipClass': 'tooltip',
+            'toolTipWidth': 200,
+            'toolTipHeight': 50,
             'contentFunction': function(element) { return $(element).attr('alt'); } // A function which returns the content for our tooltip, given the element we are processing
         };
 
         if (settings) $.extend(config, settings);
         
         if(!window.$_TOOLTIP_INFO)
-        {
-        	$('body').append('<div id="' + config['toolTipId'] + '" class="' + config['toolTipClass'] + '" />');
+            window.$_TOOLTIP_INFO = [];
+
+        $('body').append('<div id="' + config['toolTipId'] + '" class="' + config['toolTipClass'] + '" />');
         	
-        	window.$_TOOLTIP_INFO = {
-									obj: null,
-									offsetX: 0,
-									offsetY: 0
-								};
-	
-			$_TOOLTIP_INFO.obj = $('#' + config['toolTipId']);
-			$_TOOLTIP_INFO.obj.hide();
-		}
+        $_TOOLTIP_INFO[config['toolTipId']] = {
+			obj: null,
+			offsetX: 0,
+			offsetY: 0
+		};
+
+        $_TOOLTIP_INFO[config['toolTipId']].obj = $('#' + config['toolTipId']);
+        $_TOOLTIP_INFO[config['toolTipId']].obj.hide();
         
         this.each(function()
         {
@@ -46,18 +48,18 @@
                 content = config['contentFunction'](img);
 
 		    img.bind('mouseover', function(){
-		        $_TOOLTIP_INFO.obj.html(content);
-		        $_TOOLTIP_INFO.offsetX = ($_TOOLTIP_INFO.obj.width() + 10);
-				$_TOOLTIP_INFO.offsetY = ($_TOOLTIP_INFO.obj.height() + 10);
-		        $_TOOLTIP_INFO.obj.show();
+		        $_TOOLTIP_INFO[config['toolTipId']].obj.html(content);
+		        $_TOOLTIP_INFO[config['toolTipId']].offsetX = ($_TOOLTIP_INFO[config['toolTipId']].obj.width() + 10);
+		        $_TOOLTIP_INFO[config['toolTipId']].offsetY = ($_TOOLTIP_INFO[config['toolTipId']].obj.height() + 10);
+		        $_TOOLTIP_INFO[config['toolTipId']].obj.show();
 		    });
 		        
 		    img.bind('mousemove', function(e){
-		        $_TOOLTIP_INFO.obj.css({ 'top': (e.pageY - $_TOOLTIP_INFO.offsetY) + 'px', 'left': (e.pageX - $_TOOLTIP_INFO.offsetX) + 'px',});
+		        $_TOOLTIP_INFO[config['toolTipId']].obj.css({ 'top': (e.pageY - $_TOOLTIP_INFO[config['toolTipId']].offsetY) + 'px', 'left': (e.pageX - $_TOOLTIP_INFO[config['toolTipId']].offsetX) + 'px' });
 		    });
 		        
 		    img.bind('mouseout', function(){
-		        $_TOOLTIP_INFO.obj.hide();
+		        $_TOOLTIP_INFO[config['toolTipId']].obj.hide();
 		    });
         });
 
